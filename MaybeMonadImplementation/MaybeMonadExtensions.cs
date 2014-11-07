9c85c9a269362f39b55391dd34e94bd90a50809a
Christoph Hilty <christoph.hilty@garaio.com>
2014-11-07 12:00:42 +0100

    Initial implementation.

---
+++ MaybeMonadImplementation/MaybeMonadExtensions.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaybeMonadImplementation
+{
+    public static class MonadExtensions
+    {
+        public static Maybe<T> ToMaybe<T>(this T obj)
+        {
+            if(CompareWithNull(obj))
+                return new None<T>();
+
+            return new Some<T>(obj);
+        }
+
+        public static Maybe<TReturn> With<T, TReturn>(this T obj, Func<T, TReturn> func)
+            where T : class
+        {
+
+            if (CompareWithNull(obj))
+                return new None<TReturn>();
+
+            var result = func(obj);
+            if (CompareWithNull(result))
+                return new None<TReturn>();
+
+            return new Some<TReturn>(result);
+        }
+
+        public static Maybe<B> Bind<A, B>(this Maybe<A> m, Func<A, Maybe<B>> func)
+        {
+            var someA = m as Some<A>;
+            return someA == null ? new None<B>() : func(someA.Value);
+        }
+
+        public static Maybe<C> SelectMany<A, B, C>(this Maybe<A> a, Func<A, Maybe<B>> func, Func<A, B, C> select)
+        {
+            // correct: 
+            // return select(a.Value, a.Bind(func).Value).ToMaybe();
+
+            // more pure:
+            return a.Bind(aval =>
+                func(aval).Bind(bval =>
+                select(aval, bval).ToMaybe()));
+        }
+
+
+        public static Maybe<IEnumerable<TReturn>> With<T, TReturn>(this IEnumerable<T> list, Func<T, TReturn> selector)
+        {
+            if (CompareWithNull(list))
+                return new None<IEnumerable<TReturn>>();
+
+            return list.Filter().Select(selector).Filter().ToMaybe();
+        }
+
+        public static Maybe<IEnumerable<TReturn>> With<T, TReturn>(this Maybe<IEnumerable<T>> list, Func<T, TReturn> selector)
+        {
+            if (Equals(list, new None<IEnumerable<T>>()))
+                return new None<IEnumerable<TReturn>>();
+
+            return list.Value.Select(selector).Filter().ToMaybe();
+
+        }
+
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> list)
+        {
+            return list.Where(element => !CompareWithNull(element));
+        }
+
+        private static bool CompareWithNull<T>(T element)
+        {
+            return element == null;
+        }
+    }
+
+    public class Maybe
+    {
+        public static Maybe None
+        {
+            get { return new None(); }
+        }
+    }
+
+    public class Maybe<T> : Maybe
+    {
+        public static Maybe<T> Some
+        {
+            get { return new Some<T>(default(T)); }
+        }
+
+        public T Value { get; protected set; }
+    }
+
+    public interface INone
+    { }
+
+    public class None : Maybe, INone
+    {
+        protected bool Equals(None other)
+        {
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+
+            if (obj is INone) return true;
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return "None";
+        }
+
+        public override int GetHashCode()
+        {
+            return None.GetHashCode();
+        }
+    }
+
+
+    public class None<T> : Maybe<T>, IEquatable<T>, INone
+    {
+        public bool Equals(T other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+
+            var type = other.GetType();
+            if (type == typeof(None<>) || type == typeof(None)) return true;
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return "None";
+        }
+
+        protected bool Equals(None<T> other)
+        {
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return true;
+            if (ReferenceEquals(this, obj)) return true;
+
+            var type = obj.GetType();
+            if (type == typeof(None<>) || type == typeof(None)) return true;
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return None.GetHashCode();
+        }
+    }
+
+    public class Some<T> : Maybe<T>
+    {
+        protected bool Equals(Some<T> other)
+        {
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+
+            if (obj.GetType() != this.GetType()) return false;
+
+            return Equals(Value, ((Some<T>)obj).Value);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+        public Some(T value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ MaybeMonadTests/MaybeMonadTests.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaybeMonadImplementation;
+using NUnit.Framework;
+
+namespace MaybeMonadTests
+{
+    public class Person
+    {
+        public string Name { get;set; }
+        public string Vorname { get;set; }
+        public Person Nachbar { get; set; }
+    }
+
+    /// <summary>
+    /// Offene Punkte:
+    /// - Semantik With_SollteEineLeereListeLiefern_WennEsNullElementGibt
+    /// </summary>
+    [TestFixture]
+    public class MaybeMonadTests
+    {
+        [Test]
+        public void Maybe_SollteTypeInitializerZurVerfuegungStellen_MonadRule1()
+        {
+            var result = new Some<int>(4);
+
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        public void Maybe_SollteToMaybeImplementieren_MonadRule2()
+        {
+            var result = 4.ToMaybe();
+
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        public void Maybe_SollteBindImplementieren_MonadRule3()
+        {
+            var result = 4.ToMaybe().Bind(      a =>
+                         8.ToMaybe().Bind(      b =>
+                        "Test".ToMaybe().Bind(  c =>
+                        (a + " " + b + " " + c).ToMaybe()
+                        )));
+
+            Assert.That(result.Value, Is.EqualTo("4 8 Test"));
+        }
+
+        [Test]
+        public void Maybe_SollteBindImplementieren_MonadRule3_FallNothing()
+        {
+            var result = 4.ToMaybe().Bind(a =>
+                ((Person)null).ToMaybe().Bind(b =>
+                    "Test".ToMaybe().Bind(c =>
+                        (a + " " + b + " " + c).ToMaybe()
+                        )));
+
+            Assert.That(result.Value, Is.Null);
+        }
+
+        [Test]
+        public void Maybe_SollteSelectManyImplementieren_DamitDeklarativeLinqSyntaxVerwendetWerdenKann()
+        {
+            var result = from a in 10.ToMaybe()
+                            from b in " kleine ".ToMaybe()
+                                from c in "Programmierer".ToMaybe()
+                         select a + b + c;
+
+            Assert.That(result.Value, Is.EqualTo("10 kleine Programmierer"));
+        }
+
+        [Test]
+        public void Maybe_SollteSelectManyImplementieren_DamitDeklarativeLinqSyntaxVerwendetWerdenKann_FallNothing()
+        {
+            var result = from a in 10.ToMaybe()
+                         from b in ((string)null).ToMaybe()
+                         from c in "Programmierer".ToMaybe()
+                         select a + b + c;
+
+            Assert.That(result, Is.EqualTo(Maybe.None));
+            Assert.That(result.Value, Is.Null);
+        }
+
+        [Test]
+        public void Value_SollteKorrektenWertZurueckgebenWennEinerVorhanden()
+        {
+            var person = new Person();
+            var result = person.ToMaybe();
+
+            Assert.That(result.Value, Is.EqualTo(person));
+        }
+
+        [Test]
+        public void Value_SollteNullZurueckgebenWennKeinerVorhanden()
+        {
+            var result = ((Person)null).ToMaybe();
+
+            Assert.That(result.Value, Is.Null);
+            Assert.That(result, Is.EqualTo(Maybe<Person>.None));
+        }
+
+        [Test]
+        public void ToMaybe_SollteSomeAusNichtNullWertErzeugen()
+        {
+            var result = new Person().ToMaybe();
+
+            Assert.That(result, Is.TypeOf(typeof(Some<Person>)));
+            Assert.That(result, Is.Not.EqualTo(Maybe<Person>.None));
+        }
+
+        [Test]
+        public void ToMaybe_SollteNoneAusNichtNullWertErzeugen()
+        {
+            var result = ((Person)null).ToMaybe();
+
+            Assert.That(result, Is.TypeOf(typeof(None<Person>)));
+        }
+
+        [Test]
+        public void With_SollteVorhandenWertKorrektBinden()
+        {
+            var dummy = new Person() { Name = "Petricek" };
+
+            Assert.That(dummy.With(p => p.Name).Value, Is.EqualTo(dummy.Name));
+        }
+
+
+        [Test]
+        public void With_SollteNoneBinden_WennAusgangsObjektNullIst()
+        {
+            var dummy = (Person)null;
+
+            Assert.That(dummy.With(p => p.Name).Value, Is.Null);
+        }
+
+        [Test]
+        public void With_SollteNoneBinden_WennPropertyNull()
+        {
+            var dummy = new Person() { Name = null };
+
+            Assert.That(dummy.With(p => p.Name).Value, Is.Null);
+        }
+
+        [Test]
+        public void None_SollteKorrekteValueTypeSemantikImplementieren()
+        {
+            Assert.That(Maybe.None, Is.EqualTo(Maybe.None));
+        }
+
+        [Test]
+        public void None_SollteKorrekteValueTypeSemantikImplementieren_WennTypUnterschiedlich()
+        {
+            Assert.That(new None<Person>(), Is.EqualTo(Maybe.None));
+        }
+
+        [Test]
+        public void None_SollteUnabhaengigVomTypImmerEqualsSein()
+        {
+            var person = ((Person) null).ToMaybe();
+
+            Assert.That(person, Is.EqualTo(Maybe.None));
+        }
+
+        [Test]
+        public void None_SollteNichtEqualsMitNullSein()
+        {
+            Assert.That(Maybe.None, Is.Not.EqualTo(null));
+            Assert.That(Maybe.None, Is.Not.Null);
+        }
+
+
+        [Test]
+        public void NoneGeneric_SollteNichtEqualsMitNullSein()
+        {
+            Assert.That(new None<Person>(), Is.Not.Null);
+        }
+
+        [Test]
+        public void With_SollteMaybeMonadeErzeugen()
+        {
+            var maybe = new List<Person>().With(d => d.Name);
+
+            Assert.That(maybe, Is.TypeOf(typeof(Some<IEnumerable<string>>)));
+        }
+
+        [Test]
+        public void With_SollteEineLeereListeLiefern_WennEsEineLeereListeIst()
+        {
+            var maybe = new List<Person>().With(d => d.Nachbar).With(p => p.Name).Value;
+
+            Assert.That(maybe, Is.Empty);
+        }
+
+        [Test]
+        public void With_SollteEineLeereListeLiefern_WennEsNullElementGibt()
+        {
+            var maybe = new List<Person>() { new Person(), new Person() }.With(d => d.Nachbar).With(p => p.Name);
+
+            Assert.That(maybe.Value, Is.Empty);
+            Assert.That(maybe.Value, Is.EqualTo(new List<Person>()));
+        }
+
+        [Test]
+        public void With_SollteNullElementeKorrektHerausfiltern()
+        {
+            var maybe = new List<Person>() { null, null }.With(d => d.Nachbar).With(p => p.Name);
+
+            Assert.That(maybe.Value, Is.Empty);
+            Assert.That(maybe.Value, Is.EqualTo(new List<string>()));
+        }
+
+        [Test]
+        public void With_SollteLeereListeLiefern_WennAlleElementeNull()
+        {
+            var maybe = new List<Person>() { new Person(), null }.With(d => d.Nachbar).With(p => p.Name);
+
+            Assert.That(maybe.Value, Is.EqualTo(new List<Person>()));
+        }
+
+        [Test]
+        public void Equals_SollteTrueSein_WennGleicheReferenz()
+        {
+            var person = new Person();
+
+            Assert.That(person.ToMaybe(), Is.EqualTo(person.ToMaybe()));
+        }
+
+        [Test]
+        public void Equals_SollteFalseSein_WennNichtGleicheReferenz()
+        {
+            Assert.That(new Person().ToMaybe(), Is.Not.EqualTo(new Person().ToMaybe()));
+        }
+
+        [Test]
+        public void ToString_SollteNoneLiefern()
+        {
+            Assert.That(((Person)null).ToMaybe().ToString(), Is.EqualTo("None"));
+        }
+
+        [Test]
+        public void ToString_SollteToStringVonObjektLiefern()
+        {
+            Assert.That(new Person().ToMaybe().ToString(), Is.EqualTo(new Person().ToString()));
+        }
+    }
+}
